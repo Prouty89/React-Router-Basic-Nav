eff7bbffd986022dfde2b59524a7eb67812bde55
Blake <bpro0821@gmail.com>
2019-07-22 16:11:39 -0500

    Set up Navigation.js, MVP for React Router Mini

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route path='/' exact component={" "} />
-    <Route path='/about' component={" "} />
-    <Route path='/contact' component={" "} />
-
+    <Switch>
+     <Route path='/' exact component={Home} />
+     <Route path='/about' component={About} />
+     <Route path='/contact' component={Contact} />
+    </Switch>
   </div>
 );
 
